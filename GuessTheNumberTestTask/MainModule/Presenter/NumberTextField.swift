c0bf848338c53d998e370bab1286b81b16edc2fd
Magomed <Magomed.nagoev@yandex.ru>
2022-06-25 10:30:53 +0300

    Create NumberTextField.swift

---
+++ GuessTheNumberTestTask/MainModule/Presenter/NumberTextField.swift
@@ -0,0 +1,54 @@
+//
+//  NumberTextField.swift
+//  GuessTheNumberTestTask
+//
+//  Created by Нагоев Магомед on 24.06.2022.
+//
+
+import UIKit
+
+final class NumberTextField: UITextField, UITextFieldDelegate {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupTextField()
+        delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+
+        let stringNumber = NSString(string: textField.text ?? "0")
+            .replacingCharacters(in: range, with: string)
+
+        if let number = Int(stringNumber),
+           number >= 1 && number <= 100 {
+            return true
+        }
+
+        if stringNumber.isEmpty {
+            return true
+        }
+
+        return false
+    }
+
+    private func setupTextField() {
+        self.isHidden = true
+        layer.cornerRadius = 10
+        layer.borderColor = UIColor.lightGray.cgColor
+        backgroundColor = .white
+        borderStyle = .roundedRect
+        returnKeyType = .done
+        textAlignment = .center
+        placeholder = "Guess the number"
+        keyboardType = .numberPad
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
