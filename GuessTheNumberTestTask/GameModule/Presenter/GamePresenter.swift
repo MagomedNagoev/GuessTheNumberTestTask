372c911f2a3e0bb77dfe98704ca9f065401b7a24
Magomed <Magomed.nagoev@yandex.ru>
2022-06-26 19:21:25 +0300

    Update GamePresenter.swift

--- GuessTheNumberTestTask/GameModule/Presenter/GamePresenter.swift
+++ GuessTheNumberTestTask/GameModule/Presenter/GamePresenter.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
+protocol GameViewProtocol: class {
+    func setDataToComputerScreen(attempts: Int,
+                                 guessNumber: Int)
+    func setDataToPlayerScreen(attempts: Int,
+                               comparisonResult: String)
+    func setDataToResultScreen(humanTries: Int,
+                               computerTries: Int,
+                               resultGame: String)
+    func setupPlayerScreen()
+    func setupComputerScreen()
+    func setupResultScreen()
+    func missClick()
+}
+
 protocol GamePresenterProtocol: class {
-    init(view: UIViewController,
+    init(view: GameViewProtocol,
          router: RouterProtocol,
          playerHuman: PlayerModel,
-         playerComputer: PlayerModel)
+         playerComputer: ComputerPlayerModel)
 
     func tapOnMainMenu()
+    func moreNumber()
+    func lessNumber()
+    func equalNumber()
+    func compareNumbers(playerGuessNumber: Int)
 }
 
 class GamePresenter: GamePresenterProtocol {
@@ -24,24 +42,131 @@ class GamePresenter: GamePresenterProtocol {
         case result
     }
 
+    // MARK: - Private properties
     private var playerHuman: PlayerModel
-    private var playerComputer: PlayerModel
-    weak private var view: UIViewController?
+    private var playerComputer: ComputerPlayerModel
+    weak private var view: GameViewProtocol?
     private var router: RouterProtocol?
-    var screenType: ScreenType = .computer
+    private (set) var screenType: ScreenType = ScreenType.computer
 
-    required init(view: UIViewController,
+    // MARK: - Initializers
+    required init(view: GameViewProtocol,
                   router: RouterProtocol,
                   playerHuman: PlayerModel,
-                  playerComputer: PlayerModel) {
+                  playerComputer: ComputerPlayerModel) {
         self.view = view
         self.router = router
         self.playerHuman = playerHuman
         self.playerComputer = playerComputer
+        initialDataForPlayers()
+    }
+
+    // MARK: - Methods
+    func initialDataForPlayers() {
+        switch screenType {
+
+        case .player:
+            view?.setDataToPlayerScreen(
+                attempts: playerHuman.attemptsToGuess,
+                comparisonResult: "")
+            view?.setupPlayerScreen()
+        case .computer:
+            view?.setDataToComputerScreen(
+                attempts: playerComputer.player.attemptsToGuess,
+                guessNumber: computerGuessesNumber())
+            view?.setupComputerScreen()
+        case .result:
+            view?.setDataToResultScreen(
+                humanTries: playerHuman.attemptsToGuess,
+                computerTries: playerComputer.player.attemptsToGuess,
+                resultGame: whoWin())
+            view?.setupResultScreen()
+        }
+
     }
 
     func tapOnMainMenu() {
         router?.initialViewController()
     }
 
+    private func computerGuessesNumber() -> Int {
+        let guessingNumber = Double(playerComputer.minGuessingNumber + playerComputer.maxGuessingNumber) / 2.0
+        return Int(round(guessingNumber))
+    }
+
+    func moreNumber() {
+        let guessingNumber = computerGuessesNumber()
+        guard playerHuman.myNumber > guessingNumber else {
+            view?.missClick()
+            return
+        }
+
+        playerComputer.player.attemptsToGuess += 1
+        playerComputer.minGuessingNumber = guessingNumber
+        view?.setDataToComputerScreen(
+            attempts: playerComputer.player.attemptsToGuess,
+            guessNumber: computerGuessesNumber())
+    }
+
+    func lessNumber() {
+        let guessingNumber = computerGuessesNumber()
+        guard playerHuman.myNumber < guessingNumber else {
+            view?.missClick()
+            return
+        }
+
+        playerComputer.player.attemptsToGuess += 1
+        playerComputer.maxGuessingNumber = guessingNumber
+        view?.setDataToComputerScreen(
+            attempts: playerComputer.player.attemptsToGuess,
+            guessNumber: computerGuessesNumber())
+    }
+
+    func equalNumber() {
+        let guessingNumber = computerGuessesNumber()
+        guard playerHuman.myNumber == guessingNumber else {
+            view?.missClick()
+            return
+        }
+
+        changeScreenType(screenType: .player)
+    }
+
+    func compareNumbers(playerGuessNumber: Int) {
+        var result = ""
+
+        switch true {
+        case playerComputer.player.myNumber > playerGuessNumber:
+            result = ComparisonOperator.more.rawValue
+        case playerComputer.player.myNumber < playerGuessNumber:
+            result = ComparisonOperator.less.rawValue
+        case playerComputer.player.myNumber == playerGuessNumber:
+            changeScreenType(screenType: .result)
+        default: break
+        }
+
+        if screenType != .result {
+            playerHuman.attemptsToGuess += 1
+            view?.setDataToPlayerScreen(
+                attempts: playerHuman.attemptsToGuess,
+                comparisonResult: result)
+        }
+    }
+
+    private func whoWin() -> String {
+        switch true {
+        case playerHuman.attemptsToGuess < playerComputer.player.attemptsToGuess:
+            return ResultType.win.rawValue
+        case playerHuman.attemptsToGuess > playerComputer.player.attemptsToGuess:
+            return ResultType.lose.rawValue
+        case playerHuman.attemptsToGuess == playerComputer.player.attemptsToGuess:
+            return ResultType.draw.rawValue
+        default: return ""
+        }
+    }
+
+    private func changeScreenType(screenType: ScreenType) {
+        self.screenType = screenType
+        initialDataForPlayers()
+    }
 }
