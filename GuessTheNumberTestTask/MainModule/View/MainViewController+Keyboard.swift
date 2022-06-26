505e82573971a93b64c212330fb95266cbc60890
Magomed <Magomed.nagoev@yandex.ru>
2022-06-26 19:23:08 +0300

    Create MainViewController+Keyboard.swift

---
+++ GuessTheNumberTestTask/MainModule/View/MainViewController+Keyboard.swift
@@ -0,0 +1,47 @@
+//
+//  MainView+Keyboard.swift
+//  GuessTheNumberTestTask
+//
+//  Created by Нагоев Магомед on 25.06.2022.
+//
+
+import UIKit
+extension MainViewController {
+
+    // MARK: - Keyboard
+
+    func setupDismissKeyboardGesture() {
+        let dismissKeyboardTap = UITapGestureRecognizer(target: self, action: #selector(viewTapped(_: )))
+        view.addGestureRecognizer(dismissKeyboardTap)
+    }
+
+    @objc func viewTapped(_ recognizer: UITapGestureRecognizer) {
+        view.endEditing(true) // resign first responder
+    }
+
+    func setupKeyboardHiding() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(sender: NSNotification) {
+        guard let userInfo = sender.userInfo,
+              let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        if enterTheNumberButtonBottomConstraint?.constant == -55 {
+
+            let keyboardHeight = keyboardFrame.cgRectValue.height
+            enterTheNumberButtonBottomConstraint?.constant = -keyboardHeight - 10
+
+            view.layoutIfNeeded()
+        }
+    }
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        enterTheNumberButtonBottomConstraint?.constant = -55
+        view.layoutIfNeeded()
+    }
+}
